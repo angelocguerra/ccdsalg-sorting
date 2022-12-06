6595f3ad528ca7ca91d7bc9af1da503396c90018
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2022-12-06 18:55:54 +0800

    Added comments and documentation

--- MCO1/MCO1.java
+++ MCO1/MCO1.java
@@ -3,22 +3,37 @@ package MCO1;
 import java.util.Scanner;
 import java.util.Random;
 
+/**
+ CCDSALG MAJOR COURSE OUTPUT 1:
+ SORTING SUFFIX ARRAYS
+
+ @author ATUTUBO, Regner
+ @author GUERRA, Jose Maria Angelo
+ @author INOCENCIO, Rey Vincent
+ @author LABABIDI, Mezen
+ */
 public class MCO1
 {
+    /**
+     * Generates a random string of size n (requested from user input)
+     * Program generates n suffixes and placed in an array
+     * Program sorts the suffixarrays lexicographically in either Selection or Merge Sort
+     */
     public static void main(String[] args)
     {
         String InputString = "";
         int nSortingAlg = 0;
 
-
+        // Request User Input for String Length
         Scanner Scan = new Scanner(System.in);
         System.out.print("Input Length: ");
         int InputLength = Scan.nextInt();
         InputString = createRandomInput(InputLength);
+        System.out.println("Input: " + InputString);
 
         String [] SuffixArr = createSuffixArray(InputString);
 
-        //User Input for choice of Sorting Algorithm
+        // Request User Input for choice of Sorting Algorithm
         System.out.println("Select your sorting Algorithm: 1 = Selection Sort, 2 = Merge Sort");
         while (nSortingAlg == 0)
         {
@@ -30,8 +45,9 @@ public class MCO1
             }
         }
 
-        //selects sorting algorithm based on user input
-        switch (nSortingAlg){
+        // Selects Sorting Algorithm based on User Input
+        switch (nSortingAlg)
+        {
             case 1:
                 SelectionSort(SuffixArr);
                 break;
@@ -40,17 +56,20 @@ public class MCO1
                 break;
         }
 
-        //displays the sorted array
+        // Displays the Sorted Array
         for (int nCount = 0; nCount < SuffixArr.length; nCount++)
         {
             System.out.println(SuffixArr[nCount]);
         }
 
-
-
         Scan.close();
     }
 
+    /**
+     * Creates an array of suffixes from an input string
+     * @param sInput - The String value to create a suffix array for
+     * @return the suffix array of the input string
+     */
     public static String[] createSuffixArray(String sInput)
     {
         int nCount;
@@ -63,6 +82,11 @@ public class MCO1
         return suffixes;
     }
 
+    /**
+     * Standard Selection Sort algorithm modified to be used for strings
+     * Sorts the given suffix array from the random input string
+     * @param Strings - Array of String values to be sorted.
+     */
     public static void SelectionSort(String[] SuffixArr)
     {
         for ( int j=0; j < SuffixArr.length-1; j++ )
@@ -80,6 +104,13 @@ public class MCO1
         }
     }
 
+    /**
+     * Sub-Phase 2 of Merge Sort Algorithm
+     * Combines and merges two sorted subarrays
+     * @param Arr1 - subarray1 to be merged
+     * @param Arr2 - subarray2 to be merged
+     * @return the new sorted and merged array
+     */
     public static String[] merge(String[] Arr1, String[] Arr2)
     {
         int m = Arr1.length;
@@ -119,6 +150,14 @@ public class MCO1
         return Arr3;
     }
 
+    /**
+     * Standard Merge Sort algorithm modified to be used for strings
+     * Sorts the given suffix array from the random input string
+     * @param Arr - suffix array of string values to be sorted
+     * @param lo - value of the smallest array index
+     * @param hi - value of the largest array index
+     * @return the new sorted and merged array
+     */
     public static String[] mergeSort(String[] Arr, int lo, int hi)
     {
         if (lo == hi)
@@ -135,6 +174,12 @@ public class MCO1
         return arr3;
     }
 
+    /**
+     * Generates a random string with only characters a,t,c,g
+     * from a given input string size
+     * @param inputSize - size of the random string
+     * @return the random input string to be suffixed and sorted
+     */
     public static String createRandomInput(int inputSize)
     {
         String charSet = "atcg";
